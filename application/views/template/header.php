30bf8f395ea0796122a0c66fe9868049b846e669
joel <softcodec@gmail.com>
2017-04-29 16:44:01 -0500

    Se integra plantilla

---
+++ application/views/template/header.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <title>Wather Guardians</title>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet" />
+        <link href="<?php echo base_url();?>assets/css/main.css" rel="stylesheet" />
+    </head>
+    <body>
---
+++ application/views/template/nav.php
@@ -0,0 +1,7 @@
+<nav id="menu">
+    <ul class="links">
+        <li><a href="<?=base_url();?>">Inicio</a></li>
+        <li><a href="<?php echo base_url(); ?>#">Generic</a></li>
+        <li><a href="<?php echo base_url(); ?>#">Elements</a></li>
+    </ul>
+</nav>
